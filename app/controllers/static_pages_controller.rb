b898fe905ec017ad295da2824af997fc0d1de788
stevehwp87 <stevehwp_87@hotmail.com>
2015-07-29 15:09:49 +0800

    Done user type page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def users_type
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root             'static_pages#home'
+  get 'users_type'    => 'static_pages#users_type'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
