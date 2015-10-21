825e88eb81f390b70f7b6826591085820abe1d19
stevehwp87 <stevehwp_87@hotmail.com>
2015-10-22 06:45:49 +0800

    minor modification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "INSPAZE Interior Design Idea Malaysia"
+    base_title = "INSPAZE Interior Design Ideas"
     if page_title.empty?
       base_title
     else
